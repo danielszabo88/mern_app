63507c0bf2486d9a067a299804d6504e39aa3f9d
danielszabo88 <63491859+danielszabo88@users.noreply.github.com>
2020-12-06 12:50:32 +0100

    ready for production mode

--- 01 - CRUD/backend/server.js
+++ 01 - CRUD/backend/server.js
@@ -4,6 +4,7 @@ const app = express()
 const port = process.env.PORT || 5000
 const mongoose = require('mongoose')
 const fruitsRouter = require('./routes/fruits')
+const path = require('path')
 
 mongoose.connect(process.env.DB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
 
@@ -13,6 +14,9 @@ db.once('open', () => console.log('Connected to DB!'))
 app.use(express.json())
 
 app.use('/api/fruits', fruitsRouter)
+
+app.use(express.static(path.join(__dirname, '../frontend/build')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
